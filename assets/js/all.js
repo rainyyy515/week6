0c0ca02cfe43bad95130cf4382aab1b90227b5da
yanwei <yanwei3623918@gmail.com>
2022-09-02 17:11:52 +0800

    Update 2022-09-02T09:11:47.795Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,4 +1,6 @@
-$(function() {
+"use strict";
+
+$(function () {
   // console.log('Hello Bootstrap5');
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
@@ -6,56 +8,53 @@ $(function() {
     grabCursor: true,
     breakpoints: {
       640: {
-        slidesPerView: 4,
+        slidesPerView: 4
       },
       768: {
-        slidesPerView: 6,
+        slidesPerView: 6
       },
       992: {
-        slidesPerView: 8,
+        slidesPerView: 8
       },
       1200: {
-        slidesPerView: 10,
-      },
-    },
+        slidesPerView: 10
+      }
+    }
   });
-
   var swiper = new Swiper('.mySwiper2', {
     // Optional parameters
     effect: 'fade',
     fadeEffect: {
       crossFade: true
     },
-    
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+      el: '.swiper-pagination'
     },
-  
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+      prevEl: '.swiper-button-prev'
+    }
   });
-
   var swiper = new Swiper(".courseSwiper", {
     slidesPerView: 2,
     spaceBetween: 16,
     grabCursor: true,
     breakpoints: {
       768: {
-        slidesPerView: 3,
+        slidesPerView: 3
       },
       992: {
-        slidesPerView: 4,
+        slidesPerView: 4
       },
       1200: {
-        slidesPerView: 5,
+        slidesPerView: 5
       },
       1400: {
-        slidesPerView: 6,
-      },
-    },
+        slidesPerView: 6
+      }
+    }
   });
 });
+//# sourceMappingURL=all.js.map
